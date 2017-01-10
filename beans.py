65293f5478017316d1e63e3fb349212676e61ad3
jgu1 <Jialiang.Gu@ucsf.edu>
2017-01-10 09:50:47 -0800

    fix chr21 bug

--- beans.py
+++ beans.py
@@ -102,7 +102,7 @@ class SNP_manhattan(object):
 class Chrom_fields(object):
     Chrom_name_list = ['chr1' ,'chr2' ,'chr3' ,'chr4' ,'chr5' ,'chr6' ,'chr7' ,'chr8' ,'chr9' ,'chr10',
                        'chr11','chr12','chr13','chr14','chr15','chr16','chr17','chr18','chr19','chr20',
-                       'chr20','chr21','chr22','chrX' ,'chrY' ]
+                       'chr21','chr22','chrX' ,'chrY' ]
     Chrom_len_dict = {}
     Chrom_len_dict['chr1']  = 249250621 
     Chrom_len_dict['chr2']  = 243199373
